b7c514c1873078f3e9aa7e169aac504ec69b0905
Daniel Richter <danielrichter@posteo.de>
2022-09-05 22:46:25 +0000

    fix copy.sh
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- copy.sh
+++ copy.sh
@@ -1,3 +1,4 @@
-cp restic-restore.service /etc/systemd/system/restic-restore.service
+cp restic-restore@.service /etc/systemd/system/restic-restore@.service
 cp restic-backup@.service /etc/systemd/system/restic-backup@.service
-cp restic-backup.timer /etc/systemd/system/restic-backup.timer
+cp restic-backup@.timer /etc/systemd/system/restic-backup@.timer
+systemctl daemon-reload
